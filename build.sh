e12db99746336218d58a2bade6dc214d11ecbfc6
genta1915 <genta1915@gmail.com>
2026-04-15 00:19:11 +0900

    build.shから初期化コマンドを外す

--- build.sh
+++ build.sh
@@ -5,4 +5,3 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py shell -c "from django.contrib.auth import get_user_model; User=get_user_model(); username='genta'; password='genta1915'; email='genta@example.com'; user, created = User.objects.get_or_create(username=username, defaults={'email': email, 'is_staff': True, 'is_superuser': True}); user.is_staff=True; user.is_superuser=True; user.set_password(password); user.save()"
