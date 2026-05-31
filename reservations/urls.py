ee1e1ea964ad978f1197df389ad1d38d2ff220df
genta1915 <genta1915@gmail.com>
2026-06-01 00:25:36 +0900

    refactor: split reservation and admin views

--- reservations/urls.py
+++ reservations/urls.py
@@ -1,28 +1,28 @@
 from django.urls import path
-from . import views
+
+from .view_modules import admin_views, reservation_views
 
 app_name = "reservations"
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("slots/<int:slot_id>/reserve/", views.reserve, name="reserve"),
-    path("thanks/", views.thanks, name="thanks"),
+    path("", reservation_views.index, name="index"),
+    path("slots/<int:slot_id>/reserve/", reservation_views.reserve, name="reserve"),
+    path("thanks/", reservation_views.thanks, name="thanks"),
     path(
         "cancel/<int:reservation_id>/",
-        views.cansel_reservation,
-        name="cansel_reservation",
+        reservation_views.cancel_reservation,
+        name="cancel_reservation",
     ),
-    path("manage/", views.manage_home, name="manage_home"),
-    path("manage/reservations/", views.reservation_list, name="reservation_list"),
-    path("slots/partial/", views.slots_partial, name="slots_partial"),
+    path("manage/", admin_views.manage_home, name="manage_home"),
+    path(
+        "manage/reservations/",
+        admin_views.reservation_list,
+        name="reservation_list",
+    ),
+    path("slots/partial/", reservation_views.slots_partial, name="slots_partial"),
     path(
         "manage/reservations/delete/<int:pk>/",
-        views.delete_reservation,
+        admin_views.delete_reservation,
         name="delete_reservation",
-    ),
-    path(
-        "manage/reservations/edit/<int:pk>/",
-        views.edit_reservation,
-        name="edit_reservation",
     ),
 ]
