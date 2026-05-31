d01f261739dc6dca39e51f7e4ba786d2f20df38c
genta1915 <genta1915@gmail.com>
2026-05-31 23:58:04 +0900

    refactor: split admin views

---
+++ reservations/view_modules/admin_views.py
@@ -0,0 +1,53 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
+
+from ..models import Reservation
+
+
+@login_required
+def manage_home(request):
+    if not request.user.is_staff:
+        return redirect("/")
+    return render(request, "manage/home.html")
+
+
+@login_required
+def reservation_list(request):
+    if not request.user.is_staff:
+        return redirect("/")
+
+    reservations = Reservation.objects.select_related("slot").all().order_by("-id")
+
+    return render(
+        request, "manage/reservation_list.html", {"reservations": reservations}
+    )
+
+
+@login_required
+def delete_reservation(request, pk):
+    if not request.user.is_staff:
+        return redirect("/")
+
+    reservation = get_object_or_404(Reservation, pk=pk)
+
+    if request.method == "POST":
+        reservation.delete()
+
+    return redirect("reservations:reservation_list")
+
+
+@login_required
+def edit_reservation(request, pk):
+    if not request.user.is_staff:
+        return redirect("/")
+
+    reservation = get_object_or_404(Reservation, pk=pk)
+
+    if request.method == "POST":
+        reservation.name = request.POST.get("name")
+        reservation.people = request.POST.get("people")
+        reservation.phone = request.POST.get("phone")
+        reservation.save()
+        return redirect("reservations:reservation_list")
+
+    return render(request, "manage/edit_reservation.html", {"reservation": reservation})
